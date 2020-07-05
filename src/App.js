dc37069e44aab269b0bd4cbf80870b5366b4b360
Morgan Oh <morgan.oh@vanderbilt.edu>
2020-07-05 10:32:16 -0400

    added style for links and adding example links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 let Hello = props => <h1>{props.children}</h1>;
 let Link = props => (
   <a href={props.href} rel="noopener" target="_blank">
-   <h2> {props.children}</h2>
+    {props.children}
   </a>
 );
 function App() {
@@ -16,17 +16,12 @@ function App() {
         <p>
          <Hello> Welcome to Re:learning Git</Hello>
         </p>
-        <Link href="/">Add me on Github!</Link>
-        <Link href="/">GitHub</Link>
-        <Link href="/">GitHub TEST AGAIN</Link>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="links">
+        <Link href="/">Morgan's GitHub</Link>
+        <Link href="/">GitHub </Link>
+        <Link href="/">BLM Site </Link>
+        <Link href="/">NAACP </Link>
+        </div>
       </header>
     </div>
   );
